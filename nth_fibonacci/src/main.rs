6bf4eb692d20a43eaa2076d7b5625b680d96da04
vili <vili.taka-anttila@whitestone.fi>
2020-08-27 20:11:23 +0300

    Code refactored

--- nth_fibonacci/src/main.rs
+++ nth_fibonacci/src/main.rs
@@ -5,29 +5,49 @@ fn main() {
 
     let mut index = String::new();
         
+    index = read_input(index);
+
+    let n = convert_string_to_u32(index);
+
+    let res = generate_nth_fibonacci(n);
+    
+    println!("Fibonacci number at index {} is: {}",n,res);
+    
+}
+
+fn read_input(mut string:String) -> String{
+
     io::stdin()
         // & means reference of a variable.
-        .read_line(&mut index)
+        .read_line(&mut string)
         .expect("Failed to read line");
+    return string;
 
-        //remove whitespace
-    let n: u32 = index
-                .trim()
-                .parse()
-                .expect("Invalid input");
+}
+
+fn convert_string_to_u32(string: String) -> u32 {
+
+    let n: u32 = string
+        .trim()
+        .parse()
+        .expect("Invalid input");
+    return n;
+
+}
+
+fn generate_nth_fibonacci(index: u32) -> u32 {
 
     let mut current = 1;
-
     let mut a;
-    let (mut b, mut total) = (1,0);
 
-    while current <= n {
+    let (mut b, mut total) = (1,0);
+    while current <= index {
         a = b;
         b = total;
         total = a + b;
         
         current += 1;
     }
-    
-    println!("Fibonacci number at index {} is: {}",n,total);
+    return total;
+
 }
